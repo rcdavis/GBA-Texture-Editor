d172fbb8245ade6f815a519af702f999a07fc416
Ren Davis <rcdavisgd@gmail.com>
2025-06-17 21:30:42 -0400

    Added glfw and blank window

--- Editor/src/Main.cpp
+++ Editor/src/Main.cpp
@@ -1,8 +1,45 @@
 
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void glfwErrorCallback(int error, const char* description) {
+    std::cerr << "GLFW Error (" << error << "): " << description << std::endl;
+}
+
 int main() {
-    std::cout << "Initializing GBA Texture Editor..." << std::endl;
+    glfwSetErrorCallback(glfwErrorCallback);
+
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(1200, 900, "GBA Texture Editor", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwSwapInterval(1);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
 
     return 0;
 }
